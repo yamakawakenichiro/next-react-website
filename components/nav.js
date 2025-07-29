596371008739ab5883df24b2d938ba807b18e083
“yamakawakenichiro” <nykenichiro@gmail.com>
2025-07-29 11:52:19 +0900

    07291152

--- components/nav.js
+++ components/nav.js
@@ -7,20 +7,43 @@ export default function Nav() {
   const toggleNav = () => {
     setNavIsOpen((prev) => !prev)
   }
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
   return (
     <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>
+          {`
+            @media (max-width: 767px) {
+              body {
+                overflow: hidden;
+                position: fixed;
+                width: 100%;
+              }
+            }
+          `}
+        </style>
+      )}
       <button className={styles.btn} onClick={toggleNav}>
-        MENU
+        <span className={styles.bar}></span>
+        <span className="sr-only">MENU</span>
       </button>
       <ul className={styles.list}>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" onClick={closeNav}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href="/about" onClick={closeNav}>
+            About
+          </Link>
         </li>
         <li>
-          <Link href="/blog">Blog</Link>
+          <Link href="/blog" onClick={closeNav}>
+            Blog
+          </Link>
         </li>
       </ul>
     </nav>
